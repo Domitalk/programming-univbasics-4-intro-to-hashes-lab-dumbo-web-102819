015d17b2e45a7a284010dcf00dd782721ad5aded
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 01:00:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,9 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  not_so_empty_has = {
+    somethingsomething = "something or another"
+  }
 end
 
 def pioneer
