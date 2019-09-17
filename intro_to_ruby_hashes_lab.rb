9a4687d56e88e785080cca06f3e9da943e042b5b
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 01:03:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ def pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator_hash = {
+    id: "2039840980982304"
+  }
 end
 
 def my_hash_creator(key, value)
